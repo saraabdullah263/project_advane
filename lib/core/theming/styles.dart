7d23e8f6fbd8584a8f0214d9d0e8055a07e0c1fa
sara <saraabdullahkhuod@gmail.com>
2026-06-17 21:28:06 +0300

    - improve style in theming - make splash screen andriod 12+

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -3,25 +3,24 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:project1/core/theming/app_colors.dart';
 
 class TextStyles {
-  static TextStyle font24Black700Meight = TextStyle(
+  static TextStyle get font24Black700Meight => TextStyle(
     fontSize: 24.sp,
     fontWeight: FontWeight.w700,
     color: Colors.black,
   );
-  static TextStyle font32Boldblue = TextStyle(
+  static TextStyle get font32Boldblue => TextStyle(
     fontSize: 32.sp,
     fontWeight: FontWeight.bold,
     color: AppColors.mainBlue,
   );
-  static TextStyle font13NormalGray = TextStyle(
+  static TextStyle get font13NormalGray => TextStyle(
     fontSize: 13.sp,
     fontWeight: FontWeight.normal,
     color: AppColors.mainGray,
   );
-  static TextStyle font16SemiboldWhite = TextStyle(
+  static TextStyle get font16SemiboldWhite => TextStyle(
     fontSize: 16.sp,
-    fontWeight: FontWeight.w300,
+    fontWeight: FontWeight.w500,
     color: Colors.white,
   );
-
 }
